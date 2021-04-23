b14b0b7d9558f5a6a37ef169f88790b05510abdb
BenoitBrebion <benoit.brebion08@orange.fr>
2021-04-23 16:34:36 +0200

    Croquis Front

---
+++ Medical Motion Capture/Medical Motion Capture/CamViewVC.swift
@@ -0,0 +1,8 @@
+//
+//  CamViewVC.swift
+//  Medical Motion Capture
+//
+//  Created by Benoit on 23/04/2021.
+//
+
+import Foundation
--- Medical Motion Capture/Medical Motion Capture/ViewController.swift
+++ Medical Motion Capture/Medical Motion Capture/ViewController.swift
@@ -9,33 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var pickerView: UIPickerView!
-    @IBOutlet weak var label: UILabel!
-    
-    var arrayFruits = ["Apple","Banana","Orange","Grapes","Watermelon"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        pickerView.dataSource = self
-        pickerView.delegate = self
+        // Do any additional setup after loading the view
     }
-
+    
     @IBAction func cancelButton(_ segue: UIStoryboardSegue) {
     }
-}
-
-extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
     
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return arrayFruits.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return arrayFruits[row]
+    @IBAction func doneButton(_ segue: UIStoryboardSegue) {
     }
 }

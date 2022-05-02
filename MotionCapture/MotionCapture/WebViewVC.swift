55b6b6b0828aec388a5d27b0852067aca12d513a
Miraha02 <hanadelcourt@gmail.com>
2022-05-02 13:36:44 +0200

    adding json animation in the app but still have problem like adding point when pressing on ipad plus sometime freeze but don't know why yet

---
+++ MotionCapture/MotionCapture/WebViewVC.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewVC.swift
+//  MotionCapture
+//
+//  Created by Hana on 02/05/2022.
+//
+
+import UIKit
+
+class WebViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
